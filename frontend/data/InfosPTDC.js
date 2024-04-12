29e118f2ac9d2ce952d84ff9218f660b04eb677b
Gabriel Morandi <gabriel.morandi@intelitrader.com.br>
2024-04-11 22:44:09 -0300

    na mao

--- frontend/data/InfosPTDC.js
+++ frontend/data/InfosPTDC.js
@@ -2,8 +2,7 @@ export const InfosPDTC = {
   principio: [
     {
       cod_principio: "P1",
-      title:
-        "P1 - Universidade popular com diversidade social e interculturalidade",
+      title: "Universidade popular com diversidade social e interculturalidade",
       description:
         "A Unifesspa nasce de demandas e lutas sociais. Foi concebida para defender e incorporar o respeito à diversidade social, cultural e ambiental e aos direitos humanos, em uma região com imensas desigualdades sociais. Legado que deve impulsionar a busca pelo reforço de seu caráter popular e intercultural, a valorização da diversidade de sua comunidade, a efetiva inclusão de estudantes de diversas origens e experiências. Neste contexto, para se promover excelência acadêmica, é essencial adotar uma política intercultural, com currículos que combatam discriminações e garantam representatividade. A interculturalidade deve permear todas as atividades universitárias, valorizando saberes diversos e fomentando a cidadania ativa. O respeito, empatia e diálogo são fundamentais para fortalecer a convivência e promover a diversidade, estimulando a expressão cultural e artística dos estudantes. A Unifesspa precisa continuar evoluindo e implementar mudanças estruturais para consolidar seu projeto de universidade inclusiva e diversa.",
       icon: "cap",
@@ -11,43 +10,42 @@ export const InfosPDTC = {
     {
       cod_principio: "P2",
       title:
-        "P2 - Universidade como espaço de aprendizado com ações Indissociáveis (ensino-pesquisa-extensão) e pautada em problemas socialmente relevantes",
+        "Universidade como espaço de aprendizado com ações Indissociáveis (ensino-pesquisa-extensão) e pautada em problemas socialmente relevantes",
       description:
         "Tradicionalmente, as universidades valorizavam a disseminação do conhecimento científico e/ou cultural como a atividade principal, mas com a evolução democrática, o foco educacional mudou para se tornar mais inclusivo e justo. Na região da Amazônia, a defesa da universidade pública reflete a busca por uma educação popular, exigindo uma revisão na política de Ensino-Pesquisa-Extensão da Unifesspa. A escuta por nós realizada destacou a importância de integrar ciência, arte e cultura para fortalecer a universidade, adaptar programas acadêmicos para melhor acesso dos alunos e reestruturar laboratórios e grupos de pesquisa. A expansão da Unifesspa e a aproximação com comunidades locais são consideradas essenciais para uma presença efetiva. No contexto atual, as universidades enfrentam desafios com impacto das tecnologias, globalização e difusão do conhecimento, o que requer reflexão sobre os novos papéis da instituição. A integração entre Ensino-Pesquisa-Extensão é crucial, adaptando-se às demandas da sociedade e promovendo inovações pedagógicas. Assim a Unifesspa de ser capaz de promover de mudanças coletivas, reforçando a integração entre membros e incentivando o diálogo com a comunidade, visando promover aprendizado contínuo e eficaz. O envolvimento de todos os agentes educacionais e a criação de espaços propícios para interação interpessoal são chave para enfrentar os desafios atuais e garantir a indissociabilidade entre Ensino-Pesquisa-Extensão.",
       icon: "cap",
     },
     {
       cod_principio: "P3",
-      title: "P3 - Universidade democrática e autônoma",
+      title: "Universidade democrática e autônoma",
       description:
         "O binômio democracia e autonomia são fundamentais para a construção de ações da Unifesspa, desde a formação de agenda até os processos decisórios. A concepção de principios rege a democracia, que está em constante mudança e é construída coletivamente em assembleias. As formas e funções da democracia incluem o diálogo e a concertação de demandas coletivas entre os diferentes grupos sociais da universidade. A participação majoritária dos representantes estudantis, dispositivos para atender demandas das minorias, democratização do planejamento financeiro e orçamentário e ampliação dos espaços presenciais para tomada de decisões também são importantes.",
       icon: "cap",
     },
     {
       cod_principio: "P4",
-      title: "P4 - Universidade com integração multicampi",
+      title: "Universidade com integração multicampi",
       description:
         "Para garantir uma experiência educacional de qualidade em todos os campi da Unifesspa, é necessário avançar na implementação de uma abordagem inclusiva que atenda às demandas dos alunos, TAEs e professores. O que requer que sejam enfrentados problemas tais como a atenção à saúde mental dos membros da comunidade acadêmica, administração participativa, infraestrutura física e orçamentária equânime, cultura, esporte e lazer. É crucial uma administração mais adaptada às particularidades locais, com representação democrática dos servidores fora do campus principal e a criação de um núcleo multicampi. Também é importante investir em infraestrutura, cultura, esporte e lazer em todos os campi. Além disso, são necessárias políticas que valorizem a diversidade étnico-racial e garantam o acesso e permanência de alunos de diferentes origens. É fundamental que a comunidade acadêmica receba suporte emocional e a organização de projetos pedagógicos aderência às realidades regionais.",
       icon: "cap",
     },
     {
       cod_principio: "P5",
-      title: "P5 - Universidade com protagonismo estudantil",
+      title: "Universidade com protagonismo estudantil",
       description:
         "O fortalecimento do protagonismo estudantil na Unifesspa requer que seja disseminada e o enraizada entre a comunidade acadêmica a concepção de que discentes são agentes ativos em seu próprio processo formativo e na transformação da universidade e da sociedade. De forma que se deve buscar empoderar os(as) discentes, encorajando-os(as) a participar ativamente da vida universitária, a tomar decisões e a se engajar em atividades extracurriculares. O protagonismo estudantil dever ser fortalecido pelo estímulo à organização autônoma de eventos e projetos, à participação em órgãos colegiados e à defesa dos interesses da categoria. É um title fundamental para a construção da Unifesspa uma vez que garante aos discentes mais voz e influência na construção de uma universidade mais democrática e inclusiva.",
       icon: "cap",
     },
     {
       cod_principio: "P6",
-      title:
-        "P6 - Universidade com gestão transparente inclusiva e participativa",
+      title: "Universidade com gestão transparente inclusiva e participativa",
       description:
         "A construção de uma Unifesspa com gestão transparente, inclusiva e participativa requer a promoção de uma cultura democrática por meio da qual as decisões sejam tomadas levando em conta a opinião e o envolvimento da comunidade acadêmica. Sendo isso imprescindível que todas as informações sobre a gestão sejam disponibilizadas de forma clara e acessível para todos os membros da comunidade acadêmica e para a sociedade em geral; as decisões devem ser tomadas considerando a participação ativa e colaborativa da comunidade acadêmica; haja a promoção de políticas e ações que garantam a inclusão e a diversidade, com equidade de oportunidades; seja propiciado o diálogo com os membros da comunidade acadêmica na busca da construção de consensos e a resolução de conflitos; exista prestação de contas as atividades e recursos, de forma transparente e acessível.",
       icon: "cap",
     },
     {
       cod_principio: "P7",
-      title: "P7 - Universidade com acolhimento saúde e qualidade de vida",
+      title: "Universidade com acolhimento saúde e qualidade de vida",
       description:
         "A noção de acolhimento está ligada à substantividade democrática, que requer respeito aos saberes e autonomia de educandos(as) e educadores(as), criticidade e eticidade nas tomadas de decisão, e a possibilidade histórica de mudança. Já a saúde é um direito social e bem coletivo que envolve bem-estar físico, mental, social e espiritual. Qualidade de vida é a percepção do indivíduo sobre a inserção na vida e envolve saúde, educação, habitação, saneamento básico e outros contextos. A atenção à saúde dos servidores e discentes está relacionada ao cuidado com o profissional, à prevenção e acompanhamento de doenças, impactando na prestação de serviços e na academia. É importante ampliar a participação democrática e promover ações de saúde que considerem a diversidade dos servidores e discentes. É necessário um orçamento que permita a ampliação da equipe multidisciplinar, além de garantir apoio psicológico e exames médicos periódicos. A comunidade universitária deve ser ouvida em suas necessidades de saúde e qualidade de vida.",
       icon: "cap",
@@ -56,153 +54,153 @@ export const InfosPDTC = {
   tema: [
     {
       cod_tema: "T1",
-      title: "T1 - Acessibilidade",
+      title: "Acessibilidade",
       icon: "Wheelchair",
     },
     {
       cod_tema: "T2",
-      title: "T2 - Acesso à Universidade",
+      title: "Acesso à Universidade",
       icon: "University",
     },
     {
       cod_tema: "T3",
-      title: "T3 - Administração",
+      title: "Administração",
       icon: "Briefcase",
     },
     {
       cod_tema: "T4",
-      title: "T4 - Cultura",
+      title: "Cultura",
       icon: "Palette",
     },
     {
       cod_tema: "T5",
-      title: "T5 - Diversidade",
+      title: "Diversidade",
       icon: "Users",
     },
     {
       cod_tema: "T6",
-      title: "T6 - Ensino de graduação",
+      title: "Ensino de graduação",
       icon: "Book",
     },
     {
       cod_tema: "T7",
-      title: "T7 - Ensino de pós-graduação",
+      title: "Ensino de pós-graduação",
       icon: "GraduationCap",
     },
     {
       cod_tema: "T8",
-      title: "T8 - Esporte",
+      title: "Esporte",
       icon: "Award",
     },
     {
       cod_tema: "T9",
-      title: "T9 - Extensão",
+      title: "Extensão",
       icon: "BookOpen",
     },
     {
       cod_tema: "T10",
-      title: "T10 - Gestão de Pessoas",
+      title: "Gestão de Pessoas",
       icon: "UserCheck",
     },
     {
       cod_tema: "T11",
-      title: "T11 - Infraestrutura",
+      title: "Infraestrutura",
       icon: "Building",
     },
     {
       cod_tema: "T12",
-      title: "T12 - Inovação",
+      title: "Inovação",
       icon: "Lightbulb",
     },
     {
       cod_tema: "T13",
-      title: "T13 - Laboratórios",
+      title: "Laboratórios",
       icon: "Flask",
     },
     {
       cod_tema: "T14",
-      title: "T14 - Lazer",
+      title: "Lazer",
       icon: "Football",
     },
     {
       cod_tema: "T15",
-      title: "T15 - Meio Ambiente",
+      title: "Meio Ambiente",
       icon: "Leaf",
     },
     {
       cod_tema: "T16",
-      title: "T16 - Permanência",
+      title: "Permanência",
       icon: "HomeHeart",
     },
     {
       cod_tema: "T17",
-      title: "T17 - Pesquisa",
+      title: "Pesquisa",
       icon: "Search",
     },
     {
       cod_tema: "T18",
-      title: "T18 - Planejamento, orçamento e finanças",
+      title: "Planejamento, orçamento e finanças",
       icon: "Clipboard",
     },
     {
       cod_tema: "T19",
-      title: "T19 - Tecnologias de informação e comunicação",
+      title: "Tecnologias de informação e comunicação",
       icon: "Cloud",
     },
   ],
   diretriz: [
     {
       cod_diretriz: "D1",
-      title: "D1 - Acolhimento, Inclusão Social e Diversidade",
+      title: "Acolhimento, Inclusão Social e Diversidade",
       description:
         "Descubra como estamos comprometidos em promover um ambiente acolhedor e inclusivo para todos os membros da nossa comunidade acadêmica.",
     },
     {
       cod_diretriz: "D2",
-      title: "D2 - Gestão Democrática e Participativa",
+      title: "Gestão Democrática e Participativa",
       description:
         "Explore nosso compromisso com uma administração transparente e inclusiva, onde todos têm voz nas decisões que moldam o futuro da universidade.",
     },
     {
       cod_diretriz: "D3",
-      title: "D3 - Gestão Ecológica e Humanizada dos Espaços",
+      title: "Gestão Ecológica e Humanizada dos Espaços",
       description:
         "Conheça como estamos comprometidos em promover práticas sustentáveis e um ambiente acolhedor, priorizando o bem-estar de nossa comunidade acadêmica e o cuidado com o meio ambiente.",
     },
     {
       cod_diretriz: "D4",
-      title: "D4 - Integração: arte, cultura, esporte, lazer e ciência ",
+      title: "Integração: arte, cultura, esporte, lazer e ciência ",
       description:
         "Saiba como promovemos a integração entre essas áreas, enriquecendo a experiência acadêmica e fortalecendo os laços comunitários.",
     },
     {
       cod_diretriz: "D5",
-      title: "D5 - Política Institucional com Transparência",
+      title: "Política Institucional com Transparência",
       description:
         "Implementando políticas transparentes para fortalecer a confiança e a participação na comunidade acadêmica.",
     },
     {
       cod_diretriz: "D6",
-      title: "D6 - Retomar a Expansão com Planejamento e Diálogo",
+      title: "Retomar a Expansão com Planejamento e Diálogo",
       description:
         "Nosso compromisso é retomar a expansão de forma sustentável, guiados pelo diálogo e um planejamento cuidadoso, visando o crescimento equitativo e inclusivo da nossa comunidade acadêmica.",
     },
     {
       cod_diretriz: "D7",
-      title: "D7 - Suporte eficiente ao ensino, à pesquisa e à extensão",
+      title: "Suporte eficiente ao ensino, à pesquisa e à extensão",
       description:
         "Oferecendo suporte eficiente e abrangente ao ensino, pesquisa e extensão para impulsionar o desenvolvimento acadêmico e social.",
     },
     {
       cod_diretriz: "D8",
-      title: "D8 - Trabalho Digno com Saúde e Qualidade de Vida",
+      title: "Trabalho Digno com Saúde e Qualidade de Vida",
       description:
         "Priorizando um ambiente de trabalho digno, onde a saúde e qualidade de vida são valores essenciais para todos os membros da comunidade acadêmica.",
     },
     {
       cod_diretriz: "D9",
       title:
-        "D9 - Vivências Formativas e a Indissociabilidade Ensino, Pesquisa e Extensão",
+        "Vivências Formativas e a Indissociabilidade Ensino, Pesquisa e Extensão",
       description:
         "Integrando experiências formativas que unem ensino, pesquisa e extensão para uma educação holística e significativa.",
     },
