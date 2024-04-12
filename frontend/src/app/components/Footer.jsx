5404865933310091fa0b0c750cdf54da4060a379
Gabriel Morandi <gabriel.morandi@intelitrader.com.br>
2024-04-12 19:13:04 -0300

    footer

--- frontend/src/app/components/Footer.jsx
+++ frontend/src/app/components/Footer.jsx
@@ -1,39 +1,19 @@
-import MenuLink from "./MenuLink"
 import Image from "next/image"
 
 export default function Footer() {
   return (
     <footer className="grid w-full py-4 mt-16 place-items-center bg-purple">
-      <div className="flex flex-col-reverse md:flex-row md:items-center md:gap-6 md:justify-between w-11/12 max-w-[1160px]">
-        <div className="flex flex-col items-start gap-2">
-          <Image
+      <div className="flex flex-col-reverse w-11/12 max-w-[1160px]">
+        <div className="flex flex-col items-start gap-2 sm:items-end sm:flex-row">
+          {/* <Image
             src={"/LogoBalao.png"}
             width={80}
             height={80}
             alt="Plataforma Democrática"
-          />
+          /> */}
           <h2 className="font-semibold text-yellow text-balance">
             © Plataforma Democrática UNIFESSPA, todos direitos reservados 2024.
           </h2>
-        </div>
-        <div className="flex flex-col gap-2 pb-4 md:pb-0">
-          <MenuLink
-            text={"Princípios Universidade"}
-            isYellow={true}
-            href={"/"}
-          />
-          <MenuLink text={"Temas Trabalhados"} isYellow={true} href={"/"} />
-          <MenuLink
-            text={"Diretrizes dos Compromissos"}
-            isYellow={true}
-            href={"/"}
-          />
-          <MenuLink
-            text={"Conheça nossos Compromissos"}
-            isYellow={true}
-            href={"/"}
-          />
-          <MenuLink text={"As Escutas"} isYellow={true} href={"/"} />
         </div>
       </div>
     </footer>
