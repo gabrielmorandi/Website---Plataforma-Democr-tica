324f50e06d04355ec1d73a728b2f88d8aac6255f
Gabriel Morandi <gabriel.morandi@intelitrader.com.br>
2024-04-29 18:40:28 -0300

    /

--- frontend/src/app/components/BalloonCard.jsx
+++ frontend/src/app/components/BalloonCard.jsx
@@ -13,11 +13,19 @@ export default function BalloonCard({ content, type }) {
       onMouseLeave={() => setIsHover(false)}
     >
       <MessageCircle className="fill-purple stroke-purple size-9" />
-      <p className="absolute top-0 left-0 w-full leading-9 text-center text-yellow font-bold">
+      <p className="absolute top-0 left-0 w-full font-bold leading-9 text-center text-yellow">
         {content}
       </p>
       <div
-        className="absolute w-max max-w-[235px] -z-50 font-semibold leading-tight text-center m text-balance opacity-0 bottom-0 bg-yellow text-purple text-sm data-[state=active]:z-50 data-[state=active]:bottom-10 data-[state=active]:opacity-100 transition-all delay-150 p-2 rounded-lg left-1/2 -translate-x-1/2"
+        className={
+          type === 1
+            ? "absolute w-max max-w-[235px] -z-50 font-semibold leading-tight text-center m text-balance opacity-0 bottom-0 bg-pink text-blue text-sm data-[state=active]:z-50 data-[state=active]:bottom-10 data-[state=active]:opacity-100 transition-all delay-150 p-2 rounded-lg left-1/2 -translate-x-1/2"
+            : type === 2
+            ? "absolute w-max max-w-[235px] -z-50 font-semibold leading-tight text-center m text-balance opacity-0 bottom-0 bg-pink text-blue text-sm data-[state=active]:z-50 data-[state=active]:bottom-10 data-[state=active]:opacity-100 transition-all delay-150 p-2 rounded-lg left-1/2 -translate-x-1/2"
+            : type === 3
+            ? " absolute w-max max-w-[235px] -z-50 font-semibold leading-tight text-center m text-balance opacity-0 bottom-0 bg-pink text-blue text-sm data-[state=active]:z-50 data-[state=active]:bottom-10 data-[state=active]:opacity-100 transition-all delay-150 p-2 rounded-lg right-0"
+            : null
+        }
         data-state={isHover ? "active" : "inactive"}
       >
         {type && type === 1
